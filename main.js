f39b0808f6bd4bb4e007c3f217c8dd5324607de5
Soham <solutionnoton@gmail.com>
2022-05-12 21:21:18 +0530

    Added image files and scripts

---
+++ main.js
@@ -0,0 +1,48 @@
+$('.carousel').carousel({
+    interval:3000
+  })
+
+
+
+
+
+
+
+
+
+  jQuery(document).ready(function() {
+    function count($this){
+    var current = parseInt($this.html(), 10);
+    current = current + 1; /* Where 1 is increment */
+    $this.html(++current);
+    if(current > $this.data('count')){
+    $this.html($this.data('count'));
+    } else {
+    setTimeout(function(){count($this)}, 50);
+    }
+    }
+    
+    jQuery(".count-up").each(function() {
+    jQuery(this).data('count', parseInt(jQuery(this).html(), 10));
+    jQuery(this).html('0');
+    count(jQuery(this));
+    });
+
+    
+    jQuery(".count1").each(function() {
+      jQuery(this).data('count', parseInt(jQuery(this).html(), 10));
+      jQuery(this).html('0');
+      count(jQuery(this));
+      });
+
+
+      
+    jQuery(".count2").each(function() {
+      jQuery(this).data('count', parseInt(jQuery(this).html(), 10));
+      jQuery(this).html('0');
+      count(jQuery(this));
+      });
+
+
+
+    });
